e9b0e1e1985eb8bced62fa2cc59dcd2c33a61931
Athos Nunes Kolling <athoskolling@gmail.com>
2026-05-06 20:11:28 -0300

    pretooooooooooooooooooooooooooooooooooooooooo

---
+++ code1062.py
@@ -0,0 +1,5 @@
+print ("Enter the amount of lines you want to test for squares: ")
+lines = input()
+if lines != int:
+    print("Enter a valid NUMBER for the amout of lines you want to test: ")
+    lines = input()
